377b7a01a166b504eaceb2eafe6b5b47b1825148
Timothy Song <tsong0220@gmail.com>
2018-03-05 14:49:14 -0700

    added app.get using filter method to find by ID

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 'use strict';
 
 // TEMP: Simple In-Memory Database
+const express = require('express');
 const data = require('./db/notes');
 
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+const app = express();
+app.use(express.static('public'));
+
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+
+app.get('/api/notes/:id', (req, res) => {
+  const {id} = req.params;
+
+  let foundId = data.find(item => item.id === Number(id));
+  
+  console.log(foundId);
+  res.json(foundId);
+
+});
