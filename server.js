45fbcaf939cb57020c169a5e096110c0c1609018
Timothy Song <tsong0220@gmail.com>
2018-03-05 22:03:54 -0700

    moved app.list method to the last

--- server.js
+++ server.js
@@ -18,16 +18,28 @@ app.listen(8080, function () {
 });
 
 app.get('/api/notes', (req, res) => {
+//   const queryString = api.search({hi:'bye'});
+//   console.log(queryString);
   res.json(data);
+//   const searchTerm = req.query.searchTerm; if (searchTerm) 
+//   { let filteredList = data.filter(function(item) { return item.title.includes(searchTerm); });
+
 });
 
 
 app.get('/api/notes/:id', (req, res) => {
   const {id} = req.params;
 
-  let foundId = data.find(item => item.id === Number(id));
+  let foundNote = data.find(item => item.id === Number(id));
   
-  console.log(foundId);
-  res.json(foundId);
+  //   console.log(foundId);
+  res.json(foundNote);
 
 });
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+  
