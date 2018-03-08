4cb930415995401d6cf61247f419dc85c812a05c
Timothy Song <tsong0220@gmail.com>
2018-03-08 15:11:48 -0700

    update notes.method() to use promises

--- router/notes.router.js
+++ router/notes.router.js
@@ -8,62 +8,84 @@ const simDB = require('../db/simDB');
 const notes = simDB.initialize(data);
 
 router.get('/api/notes', (req, res, next) => {
-  const {searchTerm} = req.query; 
-  
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); 
-    }
-    res.json(list);
-  });
+  const { searchTerm } = req.query;
+
+  // notes.filter(searchTerm, (err, list) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   res.json(list);
+  // });
+  notes.filter(searchTerm)
+    .then(list => res.json(list))
+    .catch(err =>
+      next(err));
+
 });
-  
-  
+
+
+
+
 router.get('/api/notes/:id', (req, res, next) => {
   const { id } = req.params;
   //   let foundNote = data.find(item => item.id === Number(id));
   //   res.json(foundNote);
-  
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      return next('not found');
-    }
-  });
-});
-  
-router.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-    
-  /***** Never trust users - validate input *****/
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-    
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-    
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
+
+  notes.find(id).then(item => {
     if (item) {
       res.json(item);
     } else {
       next();
     }
-  });
+  })
+    .catch(err => {
+      next(err);
+    });
 });
+
+router.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  // notes.update(id, updateObj, (err, item) => {
+  //   if (err) {
+  //     return next(err);
+  //   }
+  //   if (item) {
+  //     res.json(item);
+  //   } else {
+  //     next();
+  //   }
+  // });
+  notes.update(id, updateObj).then(item => {
+
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  })
+    .catch(err => {
+      next(err);
+    });
+});
+
+
+
+
 
 router.post('/api/notes', (req, res, next) => {
   const { title, content } = req.body;
-  
+
   const newItem = { title, content };
   /***** Never trust users - validate input *****/
   if (!newItem.title) {
@@ -71,27 +93,29 @@ router.post('/api/notes', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-  
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
+
+
+  notes.create(newItem).then(item => {
+
     if (item) {
       res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
     } else {
       next();
     }
-  });
+  })
+    .catch(err => {
+      if (err) {
+        return next(err);
+      }
+    });
 });
 
+
 router.delete('/api/notes/:id', (req, res, next) => {
-//   notes.delete(req.params.id);
-  const {id} = req.params;
+  //   notes.delete(req.params.id);
+  const { id } = req.params;
   console.log(id);
-  notes.delete(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
+  notes.delete(id).then(item => {
     if (item) {
       res.sendStatus(204);
     } else {
@@ -99,7 +123,11 @@ router.delete('/api/notes/:id', (req, res, next) => {
       err.status = 400;
       next(err);
     }
-  });
+  })
+    .catch(err => {
+      next(err);
+    });
+
 });
- 
+
 module.exports = router;
